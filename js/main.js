31d13e89e78183e4f074a262dbfd5ee112a7f779
unknown <espinoza.dev@gmail.com>
2019-11-02 19:13:50 -0300

    Back to top

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(window).on('load', function () {
+    // Loader
+    $('#loader').delay(350).fadeOut('slow');
+    $('#icon').fadeOut('slow');
+    // Init Scroll function
+    onScroll();
+});
+
+$(function () {
+    // Smoth Scroll
+    $(document).on('scroll', onScroll);
+
+    $('.back-to-top').on('touchstart click', function () {
+        $(window).scrollTo('#top', 1000);
+    });
+});
+
+// Detects current section when scroll
+function onScroll() {
+    let headerHeight = $('header').height();
+    let scrollPosition = $(document).scrollTop() + headerHeight;
+    let refElement = $('.black-block');
+    if ($(window).scrollTop() + headerHeight - 75 > headerHeight) {
+        $('.back-to-top').addClass('show');
+    } else {
+        $('.back-to-top').removeClass('show');
+    }
+    console.log(headerHeight,'headerHeight');
+    console.log(scrollPosition,'scrollPosition');
+    console.log(refElement.position().top,'block');
+    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('.back-to-top').addClass('go-white');
+    } else {
+        $('.back-to-top').removeClass('go-white');
+    }
+}
